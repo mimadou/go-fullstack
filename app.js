c5f89071853de1e379c3af6522a2f2050387cb0c
mamadou <mimadou97@yahoo.com>
2023-10-24 16:34:19 +0200

    installation de mongodb mongoose

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://mamadou:mamadou1@cluster0.aoyawau.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
 
