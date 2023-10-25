a6532c46fa60e1475c04cbf4a4d462afdb86509e
mamadou <mimadou97@yahoo.com>
2023-10-25 21:36:13 +0200

    enregistrement et recupération des données

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const app = express();
+const Thing = require('./models/Thing');
+
 
 mongoose.connect('mongodb+srv://mamadou:mamadou1@cluster0.aoyawau.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -9,7 +10,9 @@ mongoose.connect('mongodb+srv://mamadou:mamadou1@cluster0.aoyawau.mongodb.net/?r
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use(express.json());
+  
+const app = express();
+
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -18,33 +21,23 @@ app.use((req, res, next) => {
     next();
   });
 
-app.post('/api/stuff',(req , res, next)=>{
-    console.log(req.body)
-    res.status(201).json({message : 'objet créé'})
-    next();
+  app.use(express.json());
+
+  app.post('/api/stuff',(req , res, next)=>{
+    
+    delete req.body._id;
+    const thing = new Thing ({
+        ...req.body
+    });
+    thing.save()
+    .then(()=> res.status(201).json({message:"objet enregistré"}))
+    .catch(error =>res.status(400).json({error}))
 })
 
 app.get('/api/stuff',(req , res, next)=>{
-    const stuff = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-          },
-          {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-          },
-    ]
-
-    res.status(200).json(stuff)
+   Thing.find()
+   .then(things=> res.status(200).json(things))
+   .catch(error =>res.status(400).json({error}))
 })
 
 
