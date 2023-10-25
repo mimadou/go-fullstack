459016b76933ce79a672c68686d44ff116874fba
mamadou <mimadou97@yahoo.com>
2023-10-25 22:29:56 +0200

    suppression et modification de données

--- app.js
+++ app.js
@@ -34,6 +34,18 @@ app.use((req, res, next) => {
     .catch(error =>res.status(400).json({error}))
 })
 
+app.put('/api/stuff/:id' , (req , res, next) =>{
+    Thing.updateOne({_id  :req.params.id} , {...req.body , _id : req.params.id})
+    .then(() =>res.status(200).json({message : "objet modifié"}))
+    .catch(error => res.status(400).json({error}))
+})
+
+app.delete('/api/stuff/:id', (req , res , next) =>{
+    Thing.deleteOne({ _id : req.params.id})
+    .then(() => res.status(200).json({message : 'objet supprimé'}))
+    .catch(error => req.status(400).json({error}))
+})
+
 app.get('/api/stuff/:id' ,( req , res , next) => {
      Thing.findOne({_id : req.params.id})
      .then(thing=>res.status(200).json(thing))
