e015cdf654cb7694880f69f27a0f4b8fc3d98288
mamadou <mimadou97@yahoo.com>
2023-10-29 20:29:34 +0100

    creation des controllers

--- routes/stuff.js
+++ routes/stuff.js
@@ -3,42 +3,17 @@ const express = require('express')
 const router = express();
 
 
-const Thing = require('../models/Thing');
+const stuffCtrl = require('../controllers/stuff')
 
-  router.post('/',(req , res, next)=>{
-    
-    delete req.body._id;
-    const thing = new Thing ({
-        ...req.body
-    });
-    thing.save()
-    .then(()=> res.status(201).json({message:"objet enregistré"}))
-    .catch(error =>res.status(400).json({error}))
-})
+  router.post('/', stuffCtrl.createThing)
 
-router.put('/:id' , (req , res, next) =>{
-    Thing.updateOne({_id  :req.params.id} , {...req.body , _id : req.params.id})
-    .then(() =>res.status(200).json({message : "objet modifié"}))
-    .catch(error => res.status(400).json({error}))
-})
+router.put('/:id' , stuffCtrl.modifyThing)
 
-router.delete('/:id', (req , res , next) =>{
-    Thing.deleteOne({ _id : req.params.id})
-    .then(() => res.status(200).json({message : 'objet supprimé'}))
-    .catch(error => req.status(400).json({error}))
-})
+router.delete('/:id', stuffCtrl.deleteThing)
 
-router.get('/:id' ,( req , res , next) => {
-     Thing.findOne({_id : req.params.id})
-     .then(thing=>res.status(200).json(thing))
-     .catch(error => res.status(400).json({error}))
-})
+router.get('/:id' , stuffCtrl.getOneThing)
 
-router.get('/',(req , res, next)=>{
-   Thing.find()
-   .then(things=> res.status(200).json(things))
-   .catch(error =>res.status(400).json({error}))
-})
+router.get('/', stuffCtrl.getAllThing)
 
 
 module.exports = router;
