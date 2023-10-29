50db940dc9fd559b3e40398f7eb7821ec944003b
mamadou <mimadou97@yahoo.com>
2023-10-29 20:00:49 +0100

    configuration des routes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const Thing = require('./models/Thing');
+const stuffRoutes = require('./routes/stuff')
 
 
 mongoose.connect('mongodb+srv://mamadou:mamadou1@cluster0.aoyawau.mongodb.net/?retryWrites=true&w=majority',
@@ -23,42 +23,7 @@ app.use((req, res, next) => {
 
   app.use(express.json());
 
-  app.post('/api/stuff',(req , res, next)=>{
-    
-    delete req.body._id;
-    const thing = new Thing ({
-        ...req.body
-    });
-    thing.save()
-    .then(()=> res.status(201).json({message:"objet enregistré"}))
-    .catch(error =>res.status(400).json({error}))
-})
-
-app.put('/api/stuff/:id' , (req , res, next) =>{
-    Thing.updateOne({_id  :req.params.id} , {...req.body , _id : req.params.id})
-    .then(() =>res.status(200).json({message : "objet modifié"}))
-    .catch(error => res.status(400).json({error}))
-})
-
-app.delete('/api/stuff/:id', (req , res , next) =>{
-    Thing.deleteOne({ _id : req.params.id})
-    .then(() => res.status(200).json({message : 'objet supprimé'}))
-    .catch(error => req.status(400).json({error}))
-})
-
-app.get('/api/stuff/:id' ,( req , res , next) => {
-     Thing.findOne({_id : req.params.id})
-     .then(thing=>res.status(200).json(thing))
-     .catch(error => res.status(400).json({error}))
-})
-
-app.get('/api/stuff',(req , res, next)=>{
-   Thing.find()
-   .then(things=> res.status(200).json(things))
-   .catch(error =>res.status(400).json({error}))
-})
-
-
+   app.use('/api/stuff', stuffRoutes)
 
 
 
